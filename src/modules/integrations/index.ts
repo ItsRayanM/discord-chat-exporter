24366ed139667ca56a96dd362ff121db4daf735f
Rayan Mustafa <rayanbazbuoz1@gmail.com>
2026-02-20 06:35:18 +0100

    feat(integrations): add integrations module

---
+++ src/modules/integrations/index.ts
@@ -0,0 +1,2 @@
+export { createTicketCloseHandler } from "@/modules/integrations/ticket-close.js";
+export { createAdvancedTicketCloseHandler } from "@/modules/integrations/ticket-close-advanced.js";
